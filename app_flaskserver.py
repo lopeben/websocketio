6e2464e938fe57e3a93ef03ab4ac0bd040f6ad1c
lcporquis <lcporquis@cortex.ph>
2024-07-01 15:35:36 +0800

    Cleanup and adding deployment options

--- app_flaskserver.py
+++ app_flaskserver.py
@@ -20,11 +20,11 @@ def temperature():
     return render_template('temperature.html')
 
 
+
 def generate_temperature():
     while True:
 
         try:
-
             temp = random.uniform(20.0, 30.0)  # simulated data source
             socketio.emit('temperature', {'data': str(round(temp,2))}) #, broadcast=True)
             app.logger.info("Event emitted")
@@ -35,17 +35,16 @@ def generate_temperature():
         socketio.sleep(1)
 
 
+
 if __name__ == '__main__':
     print("Staring server")
     socketio.start_background_task(generate_temperature)
 
-
     # Development Server Settings
     # socketio.run(app, debug=True, host='0.0.0.0', port=8000)
-    # socketio.run(app, debug=True, ssl_context='adhoc')
+    # socketio.run(app, debug=True, host='0.0.0.0', port=8000, allow_unsafe_werkzeug=True)
     
     # Production Server Setting
     socketio.run(app, host='0.0.0.0', port=80)
+    # socketio.run(app, host='0.0.0.0', port=5000)
     
-
-
