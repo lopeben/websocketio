3aa1452606990acc0fb3a25e47ecf8439fcfd00a
Ubuntu <ubuntu@ip-172-31-26-10.ap-southeast-1.compute.internal>
2024-07-05 06:16:11 +0000

    Render most recent data on refresh

--- app_flaskserver.py
+++ app_flaskserver.py
@@ -5,14 +5,10 @@ from flask_socketio import SocketIO, emit
 from flask import Flask, render_template
 
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 app.logger.setLevel(logging.INFO)  # or logging.DEBUG for debug level messages
-# socketio = SocketIO(app)
-# socketio = SocketIO(app, async_mode='gevent', cors_allowed_origins="*")
 socketio = SocketIO(app, async_mode='gevent', logger=True, engineio_logger=True)
-
 
 
 @app.route('/temperature')
@@ -20,31 +16,45 @@ def temperature():
     return render_template('temperature.html')
 
 
+@socketio.on('request_stored_data')
+def handle_get_initial_data():
+    try:
+        with open('data.txt', 'r') as f:
+            temp = f.read()
+    except Exception as e:
+        app.logger.error(f"Error in reading data: {e}")
+        temp = "No data available"
+    socketio.emit('temperature', {'data': temp})
+
+
+def generate_random_data():
+    while True:
+        try:
+            temp = random.uniform(20.0, 30.0)  # simulated data source
+            with open('data.txt', 'w') as f:
+                f.write(str(round(temp,2)))
+            app.logger.info("Data written to file")
+        except Exception as e:
+            app.logger.error(f"Error in write_random_data: {e}")
+        socketio.sleep(60)
+
 
 def generate_temperature():
     while True:
-
         try:
-            temp = random.uniform(20.0, 30.0)  # simulated data source
-            socketio.emit('temperature', {'data': str(round(temp,2))}) #, broadcast=True)
+            with open('data.txt', 'r') as f:
+                temp = f.read()
+            socketio.emit('temperature', {'data': temp}) #, broadcast=True)
             app.logger.info("Event emitted")
-        
         except Exception as e:
             app.logger.error(f"Error in generate_temperature: {e}")
-
-        socketio.sleep(1)
+        socketio.sleep(30)
 
 
 
 if __name__ == '__main__':
     print("Staring server")
+    socketio.start_background_task(generate_random_data)
     socketio.start_background_task(generate_temperature)
 
-    # Development Server Settings
-    # socketio.run(app, debug=True, host='0.0.0.0', port=8000)
-    # socketio.run(app, debug=True, host='0.0.0.0', port=8000, allow_unsafe_werkzeug=True)
-    
-    # Production Server Setting
     socketio.run(app, host='0.0.0.0', port=80)
-    # socketio.run(app, host='0.0.0.0', port=5000)
-    
